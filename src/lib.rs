5d91c1e52c35434a2f5e4710165018bf8493743e
Joel Courtney <joel.e.courtney@gmail.com>
2021-06-14 02:32:29 -0600

    basic lexer implementation

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,10 @@ use proc_macro::{TokenStream};
 use syn::{parse_macro_input, Attribute};
 use quote::{quote, format_ident};
 use proc_macro2::Ident;
-use proc_macro_error::{proc_macro_error, emit_error};
+use proc_macro_error::{proc_macro_error, emit_error, abort};
 use inflector::Inflector;
 use proc_macro2::TokenStream as TokenStream2;
+use parce_core::LexError;
 
 #[derive(Debug)]
 struct VariantInfo {
@@ -74,36 +75,44 @@ pub fn lex(_args: TokenStream, input: TokenStream) -> TokenStream {
     let lexer_ident = format_ident!("{}Lexer", input.ident.clone());
     let mode_idents: Vec<_> = modes.iter().map(|mode| format_ident!("{}", mode.to_class_case())).collect();
 
-    let struct_name = format!("{}PatternMatchers", input.ident.to_string());
-    let struct_ident = format_ident!("{}", struct_name);
-
     let mut pattern_matchers = vec![];
-    let mut lexemes = vec![];
     for info in &variant_info {
+        let fn_ident = format_ident!("{}", info.ident.to_string().to_snake_case());
+        let matcher = match gen_matchers(info.pattern.clone()) {
+            Ok(m) => m,
+            Err(_) => abort!(input, "it broke dude")
+        };
         pattern_matchers.push(
             quote! {
-                if let Some((data, length)) = todo!() {
-                    if let Some((_, longest_length)) = longest {
-                        if longest_length < length {
-                            longest = Some((data, length))
-                        }
-                    } else {
-                        longest = Some((data, length))
-                    }
+                fn #fn_ident(s: &str, mut start: usize) -> Option<usize> {
+                    #matcher
                 }
+
             }
         );
         if !info.fragment {
             let lexeme_ident = info.ident.clone();
-            lexemes.push(quote! {#ident::#lexeme_ident});
+            pattern_matchers.push(
+                quote! {
+                    match #fn_ident(s, start) {
+                        Some(len) => {
+                            match longest {
+                                Some((lexeme, longest_len)) if longest_len < len => {
+                                    longest = Some((#ident::#lexeme_ident, len))
+                                }
+                                None => longest = Some((#ident::#lexeme_ident, len)),
+                                _ => {}
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            )
         }
     }
 
-
     (quote! {
-        #[derive(parce_macros::LexAttributes, variants_struct::VariantsStruct, Debug)]
-        #[struct_name = #struct_name]
-        #[struct_derive(Default)]
+        #[derive(parce_macros::LexAttributes, Debug, Eq, PartialEq, Copy, Clone, Hash)]
         #input
 
         enum #lexer_ident {
@@ -116,27 +125,35 @@ pub fn lex(_args: TokenStream, input: TokenStream) -> TokenStream {
             }
         }
 
-        impl parce::lexer::Lexer<#ident> for #lexer_ident {
-            fn lex(&mut self, s: &str, start: usize) -> Result<parce::lexer::Lexeme<#ident>, parce::lexer::LexError> {
-                use parce::lexer::{Lexeme, LexError};
-                let mut longest: Option<(#ident, usize)> = None;
+        impl parce::prelude::Lexer<#ident> for #lexer_ident {
+            fn lex(&mut self, s: &str) -> Result<Vec<parce::prelude::Lexeme<#ident>>, parce::prelude::LexError> {
+                use parce::prelude::{Lexeme, LexError};
 
-                let pattern_matchers: #struct_ident<i32> = #struct_ident {
-                    // #(#pattern_matchers)*
-                    ..Default::default()
-                };
+                let mut result = vec![];
+                let mut start = 0;
+                while start < s.len() {
+                    let mut longest: Option<(#ident, usize)> = None;
 
-                for lexeme in &[#(#lexemes),*] {
-                    dbg!(pattern_matchers.get_unchecked(&lexeme));
+                    #(#pattern_matchers)*
+
+                    match longest {
+                        Some((data, len)) if len > 0 => {
+                            result.push(
+                                Lexeme {
+                                    data,
+                                    start,
+                                    len
+                                }
+                            );
+                            start += len;
+                        }
+                        _ => return Err(LexError)
+                    }
                 }
-
-                match longest {
-                    Some((data, length)) => Ok(Lexeme {
-                        data,
-                        start,
-                        length
-                    }),
-                    None => Err(LexError)
+                if start == s.len() {
+                    Ok(result)
+                } else {
+                    Err(LexError)
                 }
             }
         }
@@ -170,6 +187,270 @@ fn get_attr<'a>(s: &str, attrs: &'a Vec<Attribute>) -> Option<&'a Attribute> {
     None
 }
 
-fn lexer_discriminant(s: &str) -> TokenStream2 {
-    todo!()
+#[derive(Clone, Debug)]
+enum LexDiscriminantRule {
+    And(Vec<LexDiscriminantRule>),
+    Or(Vec<LexDiscriminantRule>),
+    Group(Vec<LexGroupRule>, bool),
+    Literal(String),
+    Lexeme(String),
+    Dot,
+    Star(Box<LexDiscriminantRule>),
+    Plus(Box<LexDiscriminantRule>),
+    Question(Box<LexDiscriminantRule>)
+}
+
+impl LexDiscriminantRule {
+    fn to_matcher(&self) -> TokenStream2 {
+        use LexDiscriminantRule::*;
+        // Heres the deal
+        // Given a rule, produce a matcher
+        // the matcher is an expression, that produces Some(len)
+        // where len is the length of the match if it finds a match
+        // or none it it doesn't
+        // match on the string s, starting at index start
+        // start is mutable, you may need to mutate it, but
+        // must be returned to its initial value before the matcher
+        // exits. This is so that star/plus/question play nicely with
+        // And/Or. Also: DO NOT USE CARRIERS
+        // using ? produces return statements on intermediate failures
+        // which we don't want
+        match self {
+            Literal(s) => {
+                let len = s.len();
+                quote! {
+                    if s.len() >= start+#len && &s[start..start+#len] == #s {
+                        Some(#len)
+                    } else {
+                        None
+                    }
+                }
+            }
+            Lexeme(l) => {
+                let lexeme_fn_ident = format_ident!("{}", l.to_snake_case());
+                quote! {
+                    #lexeme_fn_ident(s, start)
+                }
+            }
+            And(v) => {
+                let matchers: Vec<TokenStream2> = v.iter().map(|rule| rule.to_matcher()).collect();
+                quote! {
+                    let old_start = start;
+                    let result = loop {
+                        #(
+                            match {#matchers} {
+                                Some(len) => start += len,
+                                None => break None
+                            }
+                        )*
+                        break Some(start - old_start);
+                    };
+                    start = old_start;
+                    result
+                }
+            }
+            Or(v) => {
+                let matchers: Vec<_> = v.iter().map(|rule| rule.to_matcher()).collect();
+                quote! {
+                    let mut lengths = vec![];
+                    #(
+                        match {#matchers} {
+                            Some(l) => lengths.push(l),
+                            None => {}
+                        }
+                    )*
+                    lengths.max()
+                }
+            }
+            Group(g, negate) => {
+                let mut chars = vec![];
+                for rule in g {
+                    match rule {
+                        LexGroupRule::Single(c) => chars.push(c),
+                        // LexGroupRule::Range(_c1, _c2) => {}
+                    }
+                }
+                let bang = if *negate {
+                    quote! {!}
+                } else {
+                    quote! {}
+                };
+                quote! {
+                    if s.len() > start && #bang[#(#chars),*].contains(&s.chars().nth(start).unwrap()) {
+                        Some(1)
+                    } else {
+                        None
+                    }
+                }
+            }
+            Star(r) => {
+                let matcher = r.to_matcher();
+                quote! {
+                    let old_start = start;
+                    while let Some(len) = {#matcher} {
+                        start += len;
+                    }
+                    let diff = start - old_start;
+                    start = old_start;
+                    Some(diff)
+                }
+            }
+            Plus(r) => {
+                let matcher = r.to_matcher();
+                quote! {
+                    let old_start = start;
+                    let result = loop {
+                        if let Some(len) = {#matcher} {
+                            start += len;
+                        } else {
+                            break None;
+                        }
+                        while let Some(len) = {#matcher} {
+                            start += len;
+                        }
+                        break Some(start - old_start)
+                    };
+                    start = old_start;
+                    result
+                }
+            }
+            Question(r) => {
+                let matcher = r.to_matcher();
+                quote! {
+                    if let Some(len) = {#matcher} {
+                        Some(len)
+                    } else {
+                        None
+                    }
+                }
+            }
+            Dot => {
+                quote! {
+                    if s.len() > start {
+                        Some(1)
+                    } else {
+                        None
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum LexGroupRule {
+    Single(char),
+    // Range(char, char)
+}
+
+fn gen_matchers(s: String) -> Result<TokenStream2, LexError> {
+    let rule = parse_matcher(s)?;
+    Ok(rule.to_matcher())
+}
+
+fn parse_matcher(s: String) -> Result<LexDiscriminantRule, LexError> {
+    let chars: Vec<char> = s.chars().collect();
+
+    // Search for |
+    let mut i = 0;
+    let mut in_string = false;
+    let mut group_depth = 0;
+    let mut splits = vec![-1_i32];
+    while i < s.len() {
+        match chars[i] {
+            '(' | '[' if !in_string => group_depth += 1,
+            ')' | ']' if !in_string => group_depth -= 1,
+            '\'' if !in_string || chars[i-1] != '\\' => in_string = !in_string,
+            '|' if !in_string && group_depth == 0 => splits.push(i as i32),
+            _ => {}
+        }
+        i += 1;
+    }
+    if splits.len() > 1 {
+        let mut options = vec![];
+        for j in 0..splits.len()-1 {
+            options.push(parse_matcher(s[splits[j+1] as usize..splits[j+1] as usize].to_string())?);
+        }
+        return Ok(LexDiscriminantRule::Or(options));
+    }
+
+    // Generate other lexemes. If just one return it, otherwise return And.
+    let mut result = vec![];
+    i = 0;
+    while i < s.len() {
+        match chars[i] {
+            '\'' => {
+                let mut j = i + 1;
+                while j < s.len() && chars[j] != '\'' && chars[j-1] != '\\' {
+                    j += 1;
+                }
+                if j != s.len() {
+                    result.push(LexDiscriminantRule::Literal(s[i+1..j].to_string()));
+                    i = j;
+                } else {
+                    return Err(LexError);
+                }
+            }
+            '[' => {
+                let mut j = i + 1;
+                while j < s.len() && chars[j] != ']' && chars[j-1] != '\\' {
+                    j += 1;
+                }
+                if j != s.len() {
+                    let (rules, negate) = parse_group(s[i+1..j].to_string())?;
+                    result.push(LexDiscriminantRule::Group(rules, negate));
+                    i = j;
+                }
+            }
+            c if c.is_alphabetic() => {
+                let mut j = i + 1;
+                while j < s.len() && chars[j].is_alphanumeric() {
+                    j += 1;
+                }
+                result.push(LexDiscriminantRule::Lexeme(s[i..j].to_string()));
+                i = j - 1;
+            }
+            '.' => result.push(LexDiscriminantRule::Dot),
+            '*' => match result.pop() {
+                Some(l) => result.push(LexDiscriminantRule::Star(Box::new(l))),
+                None => return Err(LexError)
+            },
+            '+' => match result.pop() {
+                Some(l) => result.push(LexDiscriminantRule::Plus(Box::new(l))),
+                None => return Err(LexError)
+            },
+            '?' => match result.pop() {
+                Some(l) => result.push(LexDiscriminantRule::Question(Box::new(l))),
+                None => return Err(LexError)
+            },
+            c if c.is_whitespace() => {}
+            _ => return Err(LexError)
+        }
+        i += 1;
+    }
+    if result.len() == 0 {
+        Err(LexError)
+    } else if result.len() == 1 {
+        Ok(result.remove(0))
+    } else {
+        Ok(LexDiscriminantRule::And(result))
+    }
+}
+
+fn parse_group(s: String) -> Result<(Vec<LexGroupRule>, bool), LexError> {
+    if s.len() == 0 {
+        return Err(LexError)
+    }
+    let chars: Vec<char> = s.chars().collect();
+
+    let negate = chars[0] == '^';
+    let mut result = vec![];
+    let mut i = 0;
+    while i < s.len() {
+        match chars[i] {
+            c => result.push(LexGroupRule::Single(c))
+        }
+        i += 1;
+    }
+    Ok((result, negate))
 }
