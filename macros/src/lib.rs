6392f1d80af2592ace0fd80d4e2ec478ac2be664
JoelCourtney <joel.e.courtney@gmail.com>
2022-04-28 10:43:04 -0600

    Prefix serialized discriminants

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -10,18 +10,21 @@ use proc_macro_error::proc_macro_error;
 use lexer::LexerAst;
 use parser::ParserAst;
 
+const DISCRIMINANT_TAG: &'static str = "###PARCE-DISCRIMINANT:";
+
 #[proc_macro_error]
 #[proc_macro]
 pub fn l(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as LexerAst);
-    let text = serde_json::to_string(&ast).unwrap();
+    let text = format!("{DISCRIMINANT_TAG}{}", serde_json::to_string(&ast).unwrap());
     println!("{text}");
     (quote! { #text }).into()
 }
 
+#[proc_macro_error]
 #[proc_macro]
 pub fn p(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as ParserAst);
-    let text = dbg!(serde_json::to_string(&ast).unwrap());
+    let text = format!("{DISCRIMINANT_TAG}{}", serde_json::to_string(&ast).unwrap());
     (quote! { #text }).into()
 }
